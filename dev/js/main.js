d96dfefb4a9c4adc9e5becfb34bff2c9538a128a
jeremgg <j.33@hotmail.fr>
2018-02-12 18:14:10 +0100

    add content block animations

--- dev/js/main.js
+++ dev/js/main.js
@@ -10,4 +10,51 @@ $(document).ready(function(){
         $(this).toggleClass("active");
     });
 
+
+
+
+
+    //CLOSE MENU WHEN A LINK IS CLICKED
+    $('#menu-overlay .menu-item').click(function() {
+        $("#menu-overlay").removeClass("active");
+        $(".menu-toggle").removeClass("active");
+        $(".menu-trigger").removeClass("active");
+    });
+
+
+
+
+
+    //NAVIGATION LINK ANIMATION
+    var Page = (function() {
+        var $nav = $( '#menu-nav > a' ),
+            slitslider = $( '#slider' ).slitslider({
+                onBeforeChange : function( slide, pos ){
+                    $nav.removeClass( 'current' );
+                    $nav.eq( pos ).addClass( 'current' );
+                }
+            }),
+
+            init = function(){
+                initEvents();
+            },
+
+            initEvents = function(){
+                $nav.each( function( i ){
+                    $( this ).on( 'click', function( event ){
+                        var $dot = $( this );
+                        if( !slitslider.isActive() ){
+                            $nav.removeClass( 'current' );
+                            $dot.addClass( 'current' );
+                        }
+
+                        slitslider.jump( i + 1 );
+                        return false;
+                    });
+                });
+            };
+
+            return { init : init };
+    })();
+    Page.init();
 });
