0e3823ef8f76265370707fcd59195a27af01a2d1
jeremgg <j.33@hotmail.fr>
2018-02-13 17:49:14 +0100

    add the skills content

--- dist/js/main.js
+++ dist/js/main.js
@@ -57,4 +57,150 @@ $(document).ready(function(){
             return { init : init };
     })();
     Page.init();
+
+
+
+
+
+    //CLOSE MODAL WHEN LINK NAVIGATION IS CLICKED
+    $('#menu-overlay .menu-item').click(function(e) {
+        e.preventDefault();
+        $(".md-modal").removeClass("md-show");
+        $(".morph-button").removeClass("open");
+
+    });
+
+
+
+
+
+    //SKILLS MODAL LINKS
+    var docElem = window.document.documentElement, didScroll, scrollPosition;
+
+    // trick to prevent scrolling when opening/closing button
+    function noScrollFn() {
+        window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
+    }
+
+    function noScroll() {
+        window.removeEventListener( 'scroll', scrollHandler );
+        window.addEventListener( 'scroll', noScrollFn );
+    }
+
+    function scrollFn() {
+        window.addEventListener( 'scroll', scrollHandler );
+    }
+
+    function canScroll() {
+        window.removeEventListener( 'scroll', noScrollFn );
+        scrollFn();
+    }
+
+    function scrollHandler() {
+        if( !didScroll ) {
+            didScroll = true;
+            setTimeout( function() { scrollPage(); }, 60 );
+        }
+    };
+
+    function scrollPage() {
+        scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
+        didScroll = false;
+    };
+
+    scrollFn();
+
+    [].slice.call( document.querySelectorAll( '.morph-button' ) ).forEach( function( bttn ) {
+        new UIMorphingButton( bttn, {
+            closeEl : '.icon-close',
+            onBeforeOpen : function() {
+                // don't allow to scroll
+                noScroll();
+            },
+            onAfterOpen : function() {
+                // can scroll again
+                canScroll();
+            },
+            onBeforeClose : function() {
+                // don't allow to scroll
+                noScroll();
+            },
+            onAfterClose : function() {
+                // can scroll again
+                canScroll();
+            }
+        });
+    });
+
+
+
+
+
+    //SKILLS INTRO
+    $('.s-skills .s-nav a.next').click(function(e){
+        e.preventDefault();
+
+        var skills = $(this).parent().parent();
+        $(skills).next().animate({
+            top: '0%'
+        }, 500);
+        $(skills).removeClass('active');
+        $(skills).next().addClass('active');
+    });
+
+    $('.s-skills .s-nav a.previous').click(function(e){
+        e.preventDefault();
+
+        var skills = $(this).parent().parent();
+        $(skills).animate({
+            top: '100%'
+        }, 500);
+        $(skills).removeClass('active');
+        $(skills).prev().addClass('active');
+    })
+
+    $('#menu-overlay .menu-item').click(function(e) {
+        e.preventDefault();
+        $(".morph-button").removeClass("open");
+        $('.s-content').animate({
+            top: '100%'
+        }, 500);
+    });
+
+
+
+
+
+    //SKILLS MODAL
+    $('.morph-button button').click(function(e) {
+        e.preventDefault();
+
+        var deco = $(this).parent().find(".deco");
+        $(deco).removeClass('hide');
+        $(deco).addClass('show');
+
+        var letterBig = $(this).parent().find(".title-contact h1 span:first-child");
+        $(letterBig).removeClass('hide');
+        $(letterBig).addClass('show');
+
+        var letterSmall = $(this).parent().find(".title-contact h1 span:last-child");
+        $(letterSmall).removeClass('hide');
+        $(letterSmall).addClass('show');
+    });
+
+    $('.morph-content .icon-close').click(function(e) {
+        e.preventDefault();
+
+        var content = $(this).parent().find(".deco");
+        $(content).removeClass('show');
+        $(content).addClass('hide');
+
+        var letterBig = $(this).parent().find(".title-contact h1 span:first-child");
+        $(letterBig).removeClass('show');
+        $(letterBig).addClass('hide');
+
+        var letterSmall = $(this).parent().find(".title-contact h1 span:last-child");
+        $(letterSmall).removeClass('show');
+        $(letterSmall).addClass('hide');
+    });
 });
